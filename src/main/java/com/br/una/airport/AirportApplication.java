3ae22c8cb0f4f1af6f00de2a2db55aa6eebb3f89
Almadavic <almadavic@live.com>
2022-11-27 11:18:53 -0300

    V2.1.0

--- src/main/java/com/br/una/airport/AirportApplication.java
+++ src/main/java/com/br/una/airport/AirportApplication.java
@@ -2,8 +2,10 @@ package com.br.una.airport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class AirportApplication {
 
 	public static void main(String[] args) {
